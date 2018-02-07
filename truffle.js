f34492c04ada4c3a96c61dcb9e7074adc32fcf72
Cody Lamson <tovarishfin@gmail.com>
2018-02-07 20:34:38 +0100

    truffle init + some configs

---
+++ truffle.js
@@ -0,0 +1,23 @@
+module.exports = {
+  networks: {
+    dev: {
+      host: 'localhost',
+      port: 9545,
+      network_id: 4447
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 3,
+      gas: 4600000,
+      gasPrice: 30e9
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 42,
+      gas: 4700000,
+      gasPrice: 20e9
+    }
+  }
+}
