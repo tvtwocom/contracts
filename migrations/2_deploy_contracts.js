63afb3f2933450c80ed894b03ddfd914e3fc112c
gGrin <ggrin@riseup.net>
2018-03-16 19:31:53 +0100

    basic deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
 const TvTwoCoin = artifacts.require('../TvTwoCoin')
 const TvTwoManager = artifacts.require('../TvTwoManager')
-
+const RaidenMicroTransferChannels = artifacts.require('../RaidenMicroTransferChannels')
 module.exports = async deployer => {
   deployer.deploy(TvTwoManager)
-  deployer.deploy(TvTwoCoin)
+  await deployer.deploy(TvTwoCoin)
+  const challengePeriod = 500
+  const trustedContracts = [] // this contracts may open and top up channels on behalf of a sender
+  deployer.deploy(RaidenMicroTransferChannels, TvTwoCoin.address, challengePeriod, trustedContracts)
 }
